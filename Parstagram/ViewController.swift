3ad88cfacf9a0225fdd164973625643cfb0ec31d
Zachary Pinto <zpinto@uci.edu>
2020-02-16 13:45:45 -0800

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Zachary Pinto on 2/16/20.
+//  Copyright © 2020 Zachary Pinto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
